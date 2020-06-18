e59129b413877448b4c037021fc3b5c5a28161c9
GuanXin193402 <18817391457@163.com>
2020-06-18 16:04:22 +0800

    second commit

--- Config.py
+++ Config.py
@@ -9,10 +9,10 @@ src_path = os.path.dirname(os.path.abspath('__file__'))  # E:\Auto_Demo\Auto_set
 
 # 接口测试用例路径
 interface_case_excel_path = f'{src_path}\data\测试用例V1.2.xlsx'
-#接口测试报告路径
-interface_result_excel_path = f'{src_path}\\report\%s测试结果.xlsx' % (test_time)
+# 接口测试报告路径
+interface_result_excel_path = f'{src_path}\\report\\{test_time}测试结果.xlsx'
 
 # 访问地址
 src_url = r'http://localhost'
 if __name__ == '__main__':
-    pass
+    print(interface_result_excel_path)
